6afa98eca63b7402957ea3e8371a02ba5558e745
sadhana <sadha.sadhana7861@gmail.com>
2022-07-13 20:36:37 +0530

    first_commit

---
+++ three.java
@@ -0,0 +1,11 @@
+git remote add origin git@github.com:Sadhana1105/Devops_practice.git
+git branch -M main
+git push -u origin master
+
+git remote add origin https://github.com/Sadhana1105/Devops_practice.git
+git branch -M main
+git push -u origin master
+
+git remote add origin https://github.com/SadhanaDevops/Devops.git
+git branch -M main
+git push -u origin master
